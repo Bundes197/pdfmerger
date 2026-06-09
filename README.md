edecd889f7dd2b7801b3dfd657169687a85e7f2a
Filip Funda <filipfunda@gmail.com>
2026-06-09 16:33:09 +0200

    feat: create blank structure for the project

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # pdfmerger
+A command line tool to merge pdfs into one file.
---
+++ src/pdfmerger/cli.py
@@ -0,0 +1,8 @@
+from pypdf import PdfWriter
+import argparse
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
