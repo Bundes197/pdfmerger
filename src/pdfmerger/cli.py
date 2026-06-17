cf7995cd150b6846cc8d450a5329a929664302d8
Filip Funda <filipfunda@gmail.com>
2026-06-17 10:44:30 +0200

    feat, refactor: refactor code into functions, add InputAction and file retrieving functionality

--- src/pdfmerger/cli.py
+++ src/pdfmerger/cli.py
@@ -1,39 +1,67 @@
 from pypdf import PdfWriter
-from argparse import ArgumentParser, Namespace
+from argparse import ArgumentParser, Namespace, Action
 from pathlib import Path
 import logging
 import time
 import sys
+import os
 
-def main():
+class InputAction(Action):
+    def __call__(self,
+                parser: ArgumentParser,
+                namespace: Namespace,
+                values: str | list[str] | None,
+                option_string: str | None = None) -> None:
+        if not hasattr(namespace, 'input_queue'):
+            namespace.input_queue = []
+
+        if option_string == '-d' or option_string == '--dir':
+            namespace.input_queue.append(('dir', values))
+            namespace.dir = values
+        elif option_string is None:
+            if isinstance(values, list):
+                for file in values:
+                    namespace.input_queue.append(('file', file))
+            else:
+                namespace.input_queue.append(('file', values))
+        
+        # Updating self.dest (set automatically when calling InputAction) with new files
+        current_files = getattr(namespace, self.dest) or []
+        if isinstance(values, list):
+            current_files.extend(values)
+        else:
+            current_files.append(values)
+        setattr(namespace, self.dest, current_files)
+
+def create_parser() -> ArgumentParser:
     parser = ArgumentParser(prog="pdfmerger",
                             description="A command line tool to merge pdfs into one file.",
                             suggest_on_error=True)
     
-    parser.add_argument('files', nargs='*', help='Input files to merge')
-    parser.add_argument('-o', default='output.pdf', help='Name of merged output file')
+    parser.add_argument('-o', '--output', default='output.pdf', help='Name of merged output file')
     parser.add_argument('-f', '--force', action='store_true', help='Overwrite output file if it already exists')
+    parser.add_argument('-P', '--password', help='Encrypt the output PDF with a password')
 
-    parser.add_argument('-d', help='Search directory for pdf files')
-    parser.add_argument('-r', action='store_true', help='Search directory recursively (requires -d)')
+    parser.add_argument('-d', '--dir', action=InputAction, help='Search directory for pdf files')
+    parser.add_argument('-r', '--recursive', action='store_true', help='Search directory recursively (requires -d)')
 
     group = parser.add_mutually_exclusive_group()
-    group.add_argument('-S', action='store_true', help='Sort searched pdf files (requires -d)')
-    group.add_argument('-R', action='store_true', help='Reverse sort searched pdf files (requires -d)')
+    group.add_argument('-S', '--sort', action='store_true', help='Sort searched pdf files (requires -d)')
+    group.add_argument('-R', '--reverse', action='store_true', help='Reverse sort searched pdf files (requires -d)')
 
     parser.add_argument('-p', '--pages', nargs='+', help='Specify page ranges to extract (e.g., 1-3 5 7-end). Applies to all inputs.')
-    parser.add_argument('-P', '--password', help='Encrypt the output PDF with a password')
 
     parser.add_argument('-l', '--log', action='store_true', help='Create output log')
     parser.add_argument('-v', '--version', action='version', version='%(prog)s 0.1.0', help='Print version of pdfmerger')
     parser.add_argument('--verbose', action='store_true', help='Provides a verbose description')
 
-    arguments: Namespace = parser.parse_args()
+    parser.add_argument('files', nargs='*', action=InputAction, help='Input files to merge')
 
+    return parser
+
+def check_arguments(arguments: Namespace) -> None:
     if arguments.log:
         timestr = time.strftime("%Y%m%d-%H%M%S")
-        log_name = f"log-{timestr}.log"
-
         log_dir = Path("logs")
         log_dir.mkdir(exist_ok=True)
 
@@ -57,47 +85,109 @@ def main():
         else:
             logging.basicConfig(level=logging.WARNING, stream=sys.stdout, format="%(levelname)s: %(message)s")
     
-    if not arguments.files and not arguments.d:
-        msg = "You must provide either input files or a search directory (-d)."
-        logging.critical(msg)
-        sys.exit(f"pdfmerger: {msg}")
-
-    if arguments.files and len(arguments.files) < 2 and not arguments.d:
-        msg = "You must provide at least 2 input files to merge."
-        logging.critical(msg)
+    if not getattr(arguments, 'files', None) and not getattr(arguments, 'dir', None):
+        msg: str = "You must provide either input files or a search directory (-d)."
+        logging.critical("check_arguments(): %s", msg)
         sys.exit(f"pdfmerger: {msg}")
         
-    if (arguments.r or arguments.S or arguments.R) and not arguments.d:
-        msg = "Arguments -r, -S, and -R require a search directory (-d)."
-        logging.critical(msg)
+    if (arguments.recursive or arguments.sort or arguments.reverse) and not getattr(arguments, 'dir', None):
+        msg: str = "Arguments -r, -S, and -R require a search directory (-d)."
+        logging.critical("check_arguments(): %s", msg)
         sys.exit(f"pdfmerger: {msg}")
 
-
-    if arguments.files:
-        for file in arguments.files:
+    if getattr(arguments, 'files', None):
+        for file in getattr(arguments, 'files', []):
             path = Path(file)
             if not path.exists():
-                msg = f"Input file '{file}' does not exist."
-                logging.critical(msg)
+                msg: str = f"Input file '{file}' does not exist."
+                logging.critical("check_arguments(): %s", msg)
                 sys.exit(f"pdfmerger: {msg}")
             if not path.is_file():
-                msg = f"'{file}' is not a valid file."
-                logging.critical(msg)
+                msg: str = f"'{file}' is not a valid file."
+                logging.critical("check_arguments(): %s", msg)
                 sys.exit(f"pdfmerger: {msg}")
             if path.suffix.lower() != '.pdf':
-                msg = f"Input file '{file}' must be a PDF."
-                logging.critical(msg)
+                msg: str = f"Input file '{file}' must be a PDF."
+                logging.critical("check_arguments(): %s", msg)
                 sys.exit(f"pdfmerger: {msg}")
 
-    logging.info("Arguments are correct.")
+    logging.info("check_arguments(): Input files are valid.")
     
-    output_path = Path(arguments.o)
+    output_path = Path(arguments.output)
     if output_path.suffix.lower() != '.pdf':
-        logging.info("Output file name does not end with .pdf, adding pdf extension")
+        logging.info("check_arguments(): Output file name does not end with .pdf, adding pdf extension.")
         output_path = output_path.with_suffix('.pdf')
-        arguments.o = str(output_path)
+        arguments.output = str(output_path)
 
-    logging.info("Output file name is valid.")
+    logging.info("check_arguments(): Output file name is valid.")
+
+    path = Path(arguments.output)
+    if path.exists() and path.is_file() and not arguments.force:
+        msg: str = f"File {path.stem} already exists, cannot overwrite file (use --force to force overwrite)."
+        logging.critical("check_arguments(): %s", msg)
+        sys.exit(f"pdfmerger: {msg}")
+    
+    logging.info("check_arguments(): Output file can be created.")
+    logging.info("check_arguments(): Arguments are correct.")
+
+def search_directory(path: Path, recursive: bool) -> list[str]:
+    found_files: list[str] = []
+
+    if recursive:
+        logging.info("search_directory(): Searching %s directory recursively.", path)
+        pass
+    else:
+        logging.info("search_directory(): Searching %s directory.", path)
+        pass
+
+    return found_files
+
+def get_all_files(arguments: Namespace) -> list[str]:
+    all_files: list[str] = []
+
+    if not hasattr(arguments, 'input_queue'):
+        logging.info("get_all_files(): Input queue for files was not created, returning empty list.")
+        return all_files
+    
+    for input_type, path in arguments.input_queue:
+        if input_type == 'file':
+            logging.info("get_all_files(): Adding %s to all files.", path)
+            all_files.append(path)
+        elif input_type == 'dir':
+            dir_files: list[str] = []
+            dir_files.extend(search_directory(Path(path), arguments.recursive))
+
+            logging.info("get_all_files(): Found %d files in directory %s.", len(dir_files), path)
+            
+            if arguments.sort:
+                logging.info("get_all_files(): Sorting files from %s directory.", path)
+                dir_files.sort()
+            elif arguments.reverse:
+                logging.info("get_all_files(): Reverse sorting files from %s directory.", path)
+                dir_files.sort(reverse=True)
+
+            logging.info("get_all_files(): Adding files from directory %s to all files.", path)
+            all_files.extend(dir_files)
+
+    return all_files
+
+def check_file_count(file_list: list[str]) -> None:
+    if (len(file_list) < 2):
+        msg: str = "You must provide at least 2 input files to merge."
+        logging.critical("check_file_count(): %s", msg)
+        sys.exit(f"pdfmerger: {msg}")
+
+def main() -> None:
+    parser = create_parser()
+
+    arguments: Namespace = parser.parse_args()
+
+    check_arguments(arguments)
+
+    all_files: list[str] = get_all_files(arguments)
+
+    check_file_count(all_files)
+
 
 if __name__ == "__main__":
     main()
